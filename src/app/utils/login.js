54b0ccd45e581a90774ca3d7b115de87695d9551
Theawesometurtl <shablammmo@gmail.com>
2022-09-14 17:14:27 -0700

    argh need to properly set up firebase lemme ignore this for now

--- src/app/utils/login.js
+++ src/app/utils/login.js
@@ -1,4 +1,53 @@
 import { globals, canvas } from '../globals.js';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, set, ref } from "firebase/database";
+import { getAuth, onAuthStateChanged } from "firebase/auth";//why did I decide to add all this?
+import { getAnalytics } from "firebase/analytics";
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getDatabase();
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAAkPI-g8MRhzDjupHlZ-NOvRLDV4j8vxk",
+  authDomain: "d-shooter-4f1cc.firebaseapp.com",
+  databaseURL: "https://d-shooter-4f1cc-default-rtdb.firebaseio.com",
+  projectId: "d-shooter-4f1cc",
+  storageBucket: "d-shooter-4f1cc.appspot.com",
+  messagingSenderId: "124296778100",
+  appId: "1:124296778100:web:594d5cc09878b016ec9c21",
+  measurementId: "G-KZ55MXP4ES"
+};
+
+// Initialize Firebase
+
+
+// Initialize Realtime Database and get a reference to the service
+
+set(ref(db, 'users/' + userId), {
+    username: 'steve'
+  });
+
 export function loginScreen() {
     canvas.style = 'display = none';
     document.getElementById('start').style.display = 'none';
@@ -19,36 +68,3 @@ export function login() {
     emailElement.value = '';
     passwordElement.value = '';
 }
-
-console.log('what I need to do now is download webpack and figure out how bundles work before\
-using the rest of firebase')
-import { initializeApp } from 'firebase/app';
-import { getDatabase, set, ref } from "firebase/database";
-import { getAuth, onAuthStateChanged } from "firebase/auth";//why did I decide to add all this?
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAAkPI-g8MRhzDjupHlZ-NOvRLDV4j8vxk",
-  authDomain: "d-shooter-4f1cc.firebaseapp.com",
-  databaseURL: "https://d-shooter-4f1cc-default-rtdb.firebaseio.com",
-  projectId: "d-shooter-4f1cc",
-  storageBucket: "d-shooter-4f1cc.appspot.com",
-  messagingSenderId: "124296778100",
-  appId: "1:124296778100:web:594d5cc09878b016ec9c21",
-  measurementId: "G-KZ55MXP4ES"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Initialize Realtime Database and get a reference to the service
-const db = getDatabase();
-set(ref(db, 'users/' + userId), {
-    username: 'steve'
-  });
-
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { start } from './app/utils/start.js';
 import { gameEnd } from './app/utils/gameEnd.js';
 import { canvas } from './app/globals.js';
+import { loginScreen } from './app/utils/login.js';
 import './game.css';
 
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-let startbtn = document.getElementById("start");
-startbtn.addEventListener("click", function() {start()});
-let gameEndbtn = document.getElementById("game-over");
-gameEndbtn.addEventListener("click", function() {gameEnd()});
+//event listeners for the buttons
+let startbtn = document.getElementById("start").addEventListener("click", function() {start()});
+let gameEndbtn = document.getElementById("game-over").addEventListener("click", function() {gameEnd()});
+let loginbtn = document.getElementById("login").addEventListener("click", function() {loginScreen()});
