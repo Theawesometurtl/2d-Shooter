46edcae45e15342be26100b2f56bf00057c02b54
Theawesometurtl <shablammmo@gmail.com>
2022-10-21 14:33:13 -0700

    I am trying to create a dist directory, lets hope I don't just delete everything

--- README.md
+++
@@ -1,2 +0,0 @@
-# firstGameJamMaybe
-I'm going to try and create a simple 2d shooter and try to add the theme absorb into it somewhere idk how this will work but I can try
--- src/app/utils/changeScore.js
+++
@@ -1,9 +0,0 @@
-import { globals } from "../globals.js";
-export function changeScore(addAmount) {
-    globals.score += addAmount;
-    if (score > highscore) {
-        globals.highscore = globals.score;
-    }
-    document.getElementById('score').innerHTML = 'Score: ' + globals.score;
-    document.getElementById('highscore').innerHTML = 'Highscore: ' + globals.highscore;
-}
